8ec72187ec49d2cc7ffc2d32bcbe7cc99b7ea5cf
vuniem131104 <niemhoalongvu@gmail.com>
2025-06-04 10:27:33 +0700

    Main Deploy

--- samples/python/agents/itinerary_generation_agent/main.py
+++ samples/python/agents/itinerary_generation_agent/main.py
@@ -51,7 +51,7 @@ def main(host, port):
             name='Itinerary Generator',
             description='Helps users create detailed travel itineraries based on their preferences and requirements.',
             tags=['generate itinerary', 'edit itinerary'],
-            examples=['Create a 100-day itinerary for a journey to Hanoi, Vietnam.',],
+            examples=['Create a 1-week itinerary for a trip to Hanoi, Vietnam.',],
         )
 
         agent_card = AgentCard(
